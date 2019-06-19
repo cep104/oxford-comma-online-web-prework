21bcc536c6b670c168bdab0c8ef50d6ed5d3b84b
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-19 16:52:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,5 @@
 def oxford_comma(array)
+array.length == 1
+array.join(",")
 
-if array.length == 2
-  array.join('and')
-  elsif
-  array.length == 3
-  array.join(',and')
-else
-  array.join(",")
-end
 end
